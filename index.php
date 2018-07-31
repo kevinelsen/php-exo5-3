0b12c096f5a3518cd928a8d26aed54a2e289446d
kevin <kevinelsen@hotmail.fr>
2018-07-31 11:37:30 +0200

    exo5

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+    <?php    //Boucle for, initialisé à 1. inférieur ou égal à 15 avec une incrémentation de 1
+      for ($floor = 1; $floor <= 15; $floor++) {
+        echo 'On y arrive presque.';
+    }
+    ?>
+   </p>
+  </body>
+</html>
